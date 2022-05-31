e9d9dd9bac4f313bc74b243b870e50e782104850
Sven Kleinhans <sven.kleinhans@gmx.de>
2022-05-31 09:54:11 +0200

    added new encryption logic

--- crypt.py
+++ crypt.py
@@ -9,9 +9,11 @@ from cryptography.hazmat.primitives import hashes
 from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
 from pikepdf import Pdf, Encryption, PasswordError
 
+MAGIC_STRING = b"ENCRYPTED"
+
 
 def gen_key(password: bytes) -> bytes:
-    """Returns the Key"""
+    """Return the Key"""
     salt = b'\x81\xd2n\xb1!\xcd\xa9\xd7n\xda\x89u\xfe`\x9a\xd8'
     kdf = PBKDF2HMAC(
         algorithm=hashes.SHA512(),
@@ -22,24 +24,33 @@ def gen_key(password: bytes) -> bytes:
     return base64.urlsafe_b64encode(kdf.derive(key_material=password))
 
 
+def get_hashed_pass(password: str) -> bytes:
+    """Return the hash of the given password"""
+    digest = hashes.Hash(hashes.SHA512())
+    digest.update(password.encode())
+    return digest.finalize()
+
+
 def make_encrypted_file(path: str) -> str:
-    """Returns new string for encrypted file"""
+    """Return new string for encrypted file"""
     name, end = os.path.splitext(os.path.basename(path))
     new_string = name + "_encrypted" + end
     return os.path.dirname(path) + "\\" + new_string
 
 
 def make_decrypted_file(path: str) -> str:
-    """Returns new string for decrypted file"""
+    """Return new string for decrypted file"""
     name = os.path.basename(path)
-    new_string = name.replace("_encrypted", "")
-    return os.path.dirname(path) + "\\" + new_string
+    if "_encrypted" in name:
+        return os.path.dirname(path) + "\\" + name.replace("_encrypted", "")
+    return path
 
 
 def check_encryption(path: str) -> bool:
     """Return True if file is encrypted"""
-    file_name = os.path.basename(path)
-    return bool("_encrypted" in file_name)
+    with open(path, "rb") as file:
+        header = file.read(len(MAGIC_STRING))
+    return MAGIC_STRING == header
 
 
 def encrypt(file: str, password: str) -> int:
@@ -48,8 +59,9 @@ def encrypt(file: str, password: str) -> int:
     if not check_encryption(file):
         try:
             with open(file, "rb") as input_file, open(make_encrypted_file(file), "wb") as out:
-                file_data = input_file.read()
-                encrypted_data = fernet.encrypt(file_data)
+                data = get_hashed_pass(password) + input_file.read()
+                encrypted_data = fernet.encrypt(data)
+                out.write(MAGIC_STRING)
                 out.write(encrypted_data)
             return 4    # File successfully encrypted
         except FileNotFoundError:
@@ -65,10 +77,13 @@ def decrypt(file: str, password: str) -> int:
     if check_encryption(file):
         try:
             with open(file, "rb") as input_file, open(make_decrypted_file(file), 'wb') as out:
-                encrypted_data = input_file.read()
+                encrypted_data = input_file.read()[len(MAGIC_STRING):]
                 decrypted_data = fernet.decrypt(encrypted_data)
-                out.write(decrypted_data)
-                return 3    # File successfully decrypted
+                hashed_pass = decrypted_data[:64]
+                if hashed_pass == get_hashed_pass(password):
+                    out.write(decrypted_data[64:])
+                    return 3    # File successfully decrypted
+                return 103  # Wrong password
         except InvalidToken:
             return 103    # Wrong password
         except FileNotFoundError:
@@ -78,7 +93,7 @@ def decrypt(file: str, password: str) -> int:
     return 105    # File is not encrypted
 
 
-def encrypt_pdf(file, password: str) -> int:
+def encrypt_pdf(file: str, password: str) -> int:
     """Encrypt PDF. Return statuscode"""
     out_path = make_encrypted_file(file)
     try:
@@ -93,7 +108,7 @@ def encrypt_pdf(file, password: str) -> int:
         return 108   # Wrong permission
 
 
-def decrypt_pdf(file, password: str) -> int:
+def decrypt_pdf(file: str, password: str) -> int:
     """Decrypt PDF. Return statuscode"""
     out_path = make_decrypted_file(file)
     try:
