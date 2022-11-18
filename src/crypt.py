4c3fc9950102bd95aa68e0fc638486ed56f81416
Sven Kleinhans <Sven.Kleinhans@centrotherm.de>
2022-11-18 12:35:17 +0100

    Added new image encryption

--- src/crypt.py
+++ src/crypt.py
@@ -2,124 +2,193 @@
 Author: Sven Kleinhans
 """
 
-import os
 import base64
+import os
+
+from PIL import Image, UnidentifiedImageError
 from cryptography.fernet import Fernet, InvalidToken
 from cryptography.hazmat.primitives import hashes
+from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
 from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
 from pikepdf import Pdf, Encryption, PasswordError
 
 MAGIC_STRING = b"ENCRYPTED"
 
 
-def gen_key(password: bytes) -> bytes:
-    """Return the Key"""
-    salt = b'\x81\xd2n\xb1!\xcd\xa9\xd7n\xda\x89u\xfe`\x9a\xd8'
-    kdf = PBKDF2HMAC(
-        algorithm=hashes.SHA512(),
-        length=32,
-        salt=salt,
-        iterations=100000
-    )
-    return base64.urlsafe_b64encode(kdf.derive(key_material=password))
+class Crypt:
+    def __init__(self, file: str, password: str, encryption: bool):
+        self.file = file
+        self.password = password
+        self.encryption = encryption
 
+    def start(self):
+        if self.encryption:
+            return self.encrypt()
+        else:
+            return self.decrypt()
 
-def get_hashed_pass(password: str) -> bytes:
-    """Return the hash of the given password"""
-    digest = hashes.Hash(hashes.SHA512())
-    digest.update(password.encode())
-    return digest.finalize()
+    def get_hashed_pass(self) -> bytes:
+        digest = hashes.Hash(hashes.SHA512())
+        digest.update(self.password.encode())
+        return digest.finalize()
 
+    def make_encrypted_file(self) -> str:
+        name, end = os.path.splitext(os.path.basename(self.file))
+        new_string = name + "_encrypted" + end
+        return os.path.dirname(self.file) + "\\" + new_string
 
-def make_encrypted_file(path: str) -> str:
-    """Return new string for encrypted file"""
-    name, end = os.path.splitext(os.path.basename(path))
-    new_string = name + "_encrypted" + end
-    return os.path.dirname(path) + "\\" + new_string
+    def make_decrypted_file(self) -> str:
+        name = os.path.basename(self.file)
+        if "_encrypted" in name:
+            return os.path.dirname(self.file) + "\\" + name.replace("_encrypted", "")
+        return self.file
 
+    def check_encryption(self) -> bool:
+        with open(self.file, "rb") as file:
+            header = file.read(len(MAGIC_STRING))
+        return MAGIC_STRING == header
 
-def make_decrypted_file(path: str) -> str:
-    """Return new string for decrypted file"""
-    name = os.path.basename(path)
-    if "_encrypted" in name:
-        return os.path.dirname(path) + "\\" + name.replace("_encrypted", "")
-    return path
+    def encrypt(self) -> int:
+        fernet = Fernet(self._gen_key())
+        if not self.check_encryption():
+            try:
+                with open(self.file, "rb") as input_file, open(self.make_encrypted_file(), "wb") as out:
+                    data = self.get_hashed_pass() + input_file.read()
+                    encrypted_data = fernet.encrypt(data)
+                    out.write(MAGIC_STRING)
+                    out.write(encrypted_data)
+                return 4  # File successfully encrypted
+            except FileNotFoundError:
+                return 101  # File does not exist
+            except PermissionError:
+                return 108  # Wrong permission
+        return 104  # File is not encrypted
 
-
-def check_encryption(path: str) -> bool:
-    """Return True if file is encrypted"""
-    with open(path, "rb") as file:
-        header = file.read(len(MAGIC_STRING))
-    return MAGIC_STRING == header
-
-
-def encrypt(file: str, password: str) -> int:
-    """Encrypt the file. Return statuscode"""
-    fernet = Fernet(gen_key(password.encode()))
-    if not check_encryption(file):
-        try:
-            with open(file, "rb") as input_file, open(make_encrypted_file(file), "wb") as out:
-                data = get_hashed_pass(password) + input_file.read()
-                encrypted_data = fernet.encrypt(data)
-                out.write(MAGIC_STRING)
-                out.write(encrypted_data)
-            return 4    # File successfully encrypted
-        except FileNotFoundError:
-            return 101   # File does not exist
-        except PermissionError:
-            return 108  # Wrong permission
-    return 104    # File is not encrypted
-
-
-def decrypt(file: str, password: str) -> int:
-    """Decrypt the file. Return statuscode"""
-    fernet = Fernet(gen_key(password.encode()))
-    if check_encryption(file):
-        try:
-            with open(file, "rb") as input_file, open(make_decrypted_file(file), 'wb') as out:
-                encrypted_data = input_file.read()[len(MAGIC_STRING):]
-                decrypted_data = fernet.decrypt(encrypted_data)
-                hashed_pass = decrypted_data[:64]
-                if hashed_pass == get_hashed_pass(password):
-                    out.write(decrypted_data[64:])
-                    return 3    # File successfully decrypted
+    def decrypt(self) -> int:
+        fernet = Fernet(self._gen_key())
+        if self.check_encryption():
+            try:
+                with open(self.file, "rb") as input_file, open(self.make_decrypted_file(), 'wb') as out:
+                    encrypted_data = input_file.read()[len(MAGIC_STRING):]
+                    decrypted_data = fernet.decrypt(encrypted_data)
+                    hashed_pass = decrypted_data[:64]
+                    if hashed_pass == self.get_hashed_pass():
+                        out.write(decrypted_data[64:])
+                        return 3  # File successfully decrypted
+                    return 103  # Wrong password
+            except InvalidToken:
                 return 103  # Wrong password
-        except InvalidToken:
-            return 103    # Wrong password
+            except FileNotFoundError:
+                return 101  # File does not exist
+            except PermissionError:
+                return 108  # Wrong permission
+        return 105  # File is not encrypted
+
+    def _gen_key(self, length: int = 32) -> bytes:
+        salt = b'\x81\xd2n\xb1!\xcd\xa9\xd7n\xda\x89u\xfe`\x9a\xd8'
+        kdf = PBKDF2HMAC(
+            algorithm=hashes.SHA512(),
+            length=length,
+            salt=salt,
+            iterations=100000
+        )
+        return base64.urlsafe_b64encode(kdf.derive(key_material=self.password.encode()))
+
+
+class PDFHandler(Crypt):
+    def __init__(self, **kwargs):
+        super(PDFHandler, self).__init__(**kwargs)
+
+    def start(self):
+        if self.encryption:
+            return self.encrypt()
+        else:
+            return self.decrypt()
+
+    def encrypt(self) -> int:
+        try:
+            with Pdf.open(os.path.normpath(self.file)) as input_file:
+                input_file.save(self.make_encrypted_file(),
+                                encryption=Encryption(owner=self.password, user=self.password))
+            return 4  # File successfully encrypted
+        except PasswordError:
+            return 104  # File already encrypted
         except FileNotFoundError:
-            return 101   # File does not exist
+            return 101  # File does not exist
         except PermissionError:
             return 108  # Wrong permission
-    return 105    # File is not encrypted
+
+    def decrypt(self) -> int:
+        try:
+            with Pdf.open(os.path.normpath(self.file), password=self.password) as input_file:
+                if input_file.is_encrypted:
+                    input_file.save(self.make_decrypted_file())
+                    return 3  # File successfully decrypted
+                return 105  # File is not encrypted
+        except PasswordError:
+            return 103  # Wrong password
+        except FileNotFoundError:
+            return 101  # File does not exist
+        except PermissionError:
+            return 108  # Wrong permission
 
 
-def encrypt_pdf(file: str, password: str) -> int:
-    """Encrypt PDF. Return statuscode"""
-    out_path = make_encrypted_file(file)
-    try:
-        with Pdf.open(os.path.normpath(file)) as input_file:
-            input_file.save(out_path, encryption=Encryption(owner=password, user=password))
-        return 4    # File successfully encrypted
-    except PasswordError:
-        return 104    # File already encrypted
-    except FileNotFoundError:
-        return 101   # File does not exist
-    except PermissionError:
-        return 108   # Wrong permission
+class ImageHandler(Crypt):
+    def __init__(self, **kwargs):
+        super(ImageHandler, self).__init__(**kwargs)
+        self.data = bytes()
 
+    def start(self):
+        return self.process_image()
 
-def decrypt_pdf(file: str, password: str) -> int:
-    """Decrypt PDF. Return statuscode"""
-    out_path = make_decrypted_file(file)
-    try:
-        with Pdf.open(os.path.normpath(file), password=password) as input_file:
-            if input_file.is_encrypted:
-                input_file.save(out_path)
-                return 3    # File successfully decrypted
-            return 105    # File is not encrypted
-    except PasswordError:
-        return 103    # Wrong password
-    except FileNotFoundError:
-        return 101   # File does not exist
-    except PermissionError:
-        return 108   # Wrong permission
+    def process_image(self):
+        try:
+            img = Image.open(self.file)
+        except FileNotFoundError:
+            return 101  # File does not exist
+        except UnidentifiedImageError:
+            return 107  # Path reference invalid
+
+        self.data = img.convert("RGB").tobytes()
+
+        original = len(self.data)
+        self.data = self.pad(self.data)
+
+        if self.encryption:
+            new_data = self.encrypt()
+            out_path = self.make_encrypted_file()
+        else:
+            new_data = self.decrypt()
+            out_path = self.make_decrypted_file()
+        new_img = self.convert_to_rgb(new_data[:original])
+
+        img2 = Image.new("RGB", img.size)
+        img2.putdata(new_img)
+        img2.save(f"{out_path}", img.format)
+
+    def encrypt(self):
+        aes = Cipher(algorithms.AES(self._gen_key(length=16)), modes.CBC(self.get_initialization_vector()))
+        encryptor = aes.encryptor()
+        new_data = encryptor.update(self.data) + encryptor.finalize()
+        return new_data
+
+    def decrypt(self):
+        aes = Cipher(algorithms.AES(self._gen_key(length=16)), modes.CBC(self.get_initialization_vector()))
+        decryptor = aes.decryptor()
+        new_data = decryptor.update(self.data) + decryptor.finalize()
+        return new_data
+
+    @staticmethod
+    def convert_to_rgb(data):
+        r, g, b = tuple(map(lambda d: [data[i] for i in range(0, len(data)) if i % 3 == d], [0, 1, 2]))
+        pixels = tuple(zip(r, g, b))
+        return pixels
+
+    @staticmethod
+    def get_initialization_vector():
+        return os.urandom(16)
+
+    @staticmethod
+    def pad(data):
+        # AES requires that plaintexts be a multiple of 16, so we have to pad the data
+        return data + b"\x00" * (16 - len(data) % 16)
--- src/main.py
+++ src/main.py
@@ -2,17 +2,19 @@
 Author: Sven Kleinhans
 Version: 1.2
 """
-from PyQt5 import QtCore
-
-from src import crypt
-import sys
 import os
+import sys
+
+from PIL import Image, UnidentifiedImageError
+from PyQt5 import QtCore
+from PyQt5.QtCore import Qt, QPropertyAnimation, QAbstractAnimation, QEasingCurve, QTranslator, QLocale, QEvent
+from PyQt5.QtGui import QIcon
+from PyQt5.QtWidgets import *
+
 from GUI import Ui_MainWindow
 from InfoDialog import Ui_Dialog
 from SettingsDialog import Ui_settings
-from PyQt5.QtWidgets import *
-from PyQt5.QtCore import Qt, QPropertyAnimation, QAbstractAnimation, QEasingCurve, QTranslator, QLocale, QEvent
-from PyQt5.QtGui import QIcon
+from crypt import Crypt, ImageHandler, PDFHandler
 
 GLOBAL_STATE = 0
 
@@ -25,18 +27,18 @@ class Settings(QDialog, Ui_settings):
         self.setAttribute(Qt.WidgetAttribute.WA_TranslucentBackground)
 
         def move_window(event):
-            """Calculate the new position of the window and move it"""
             if event.buttons() == Qt.MouseButton.LeftButton:
                 self.move(self.pos() + event.globalPos() - self.drag_pos)
                 self.drag_pos = event.globalPos()
                 event.accept()
             else:
                 event.ignore()
+
         self.header.mouseMoveEvent = move_window
 
         def window_pressed(event):
-            """Save the global position of the window"""
             self.drag_pos = event.globalPos()
+
         self.header.mousePressEvent = window_pressed
 
 
@@ -48,22 +50,21 @@ class InfoDialog(QDialog, Ui_Dialog):
         self.setAttribute(Qt.WidgetAttribute.WA_TranslucentBackground)
 
         def move_window(event):
-            """Calculate the new position of the window and move it"""
             if event.buttons() == Qt.MouseButton.LeftButton:
                 self.move(self.pos() + event.globalPos() - self.drag_pos)
                 self.drag_pos = event.globalPos()
                 event.accept()
             else:
                 event.ignore()
+
         self.header.mouseMoveEvent = move_window
 
         def window_pressed(event):
-            """Save the global position of the window"""
             self.drag_pos = event.globalPos()
+
         self.header.mousePressEvent = window_pressed
 
     def change_label_lang(self, lang: str):
-        """Set the language of the InfoLabel on language change"""
         if lang == "EN":
             self.info_text.setText(open("./UI-files/InfoLabel_EN.html", "r").read())
         elif lang == "DE":
@@ -71,18 +72,6 @@ class InfoDialog(QDialog, Ui_Dialog):
 
 
 class Ui(QMainWindow, Ui_MainWindow):
-    """
-    Methods
-    -------
-    setup_ui():
-        Setup UI and make definitions
-
-    maximize_restore():
-        Maximize the Window or restore the default size
-
-    animate_sidemenu():
-        Animate the Sidemenu
-    """
     def __init__(self):
         super(Ui, self).__init__()
         self.setupUi(self)
@@ -102,22 +91,21 @@ class Ui(QMainWindow, Ui_MainWindow):
         self.setup_ui()
 
         def move_window(event):
-            """Calculate the new position of the window and move it"""
             if event.buttons() == Qt.MouseButton.LeftButton:
                 self.move(self.pos() + event.globalPos() - self.drag_pos)
                 self.drag_pos = event.globalPos()
                 event.accept()
             else:
                 event.ignore()
+
         self.header.mouseMoveEvent = move_window
 
         def window_pressed(event):
-            """Save the global position of the window"""
             self.drag_pos = event.globalPos()
+
         self.header.mousePressEvent = window_pressed
 
     def setup_ui(self) -> None:
-        """Setup UI and make definitions"""
         self.pass_input.textChanged.connect(lambda: self.start_button.setEnabled(True))
         self.slider.clicked.connect(self.animate_sidemenu)
         self.setWindowIcon(QIcon(r"./UI-files/icons/logo.png"))
@@ -130,7 +118,6 @@ class Ui(QMainWindow, Ui_MainWindow):
         self.show()
 
     def maximize_restore(self) -> None:
-        """Maximize the Window or restore the default size"""
         global GLOBAL_STATE
         if GLOBAL_STATE == 1:
             self.showNormal()
@@ -146,7 +133,6 @@ class Ui(QMainWindow, Ui_MainWindow):
             self.main_frame.setStyleSheet(stylesheet)
 
     def animate_sidemenu(self) -> None:
-        """Animate the Sidemenu"""
         if self.side_menu.frameGeometry().width() > 60:
             # Close Sidemenu
             start_value = self.side_menu.frameGeometry().width()
@@ -182,52 +168,6 @@ class Ui(QMainWindow, Ui_MainWindow):
 
 
 class Main:
-    """
-    worker():
-        The worker for the encryption and decryption
-
-    get_inputs():
-        Create the input paths and get the password
-
-    sort_input(input_list):
-        Generate a file list from the input paths
-
-    search_in_folder(paths):
-        Search for files in folders
-
-    extend_dir(path_list, path):
-        Return a list with the full names of the files in the folder
-
-    get_password():
-        Get password from user input
-
-    check_password(password):
-        Return true if the password is valid
-
-    check_enc_or_dec():
-        Return the operation
-
-    messages(num, file):
-        Create the message
-
-    set_colour(colour, msg):
-        Set the colour of the message
-
-    clear_all():
-        Clear all for the next run
-
-    remove_all_items():
-        Remove all items from the QListWidget
-
-    remove_selected_items():
-        Remove all selected items from the QListWidget
-
-    show_info():
-        Show the Info Dialog
-
-    show_settings():
-        Show the Settings Dialog
-    """
     def __init__(self):
         self.file_list = []
         self.password = ""
@@ -242,31 +182,30 @@ class Main:
         self.ui.close.clicked.connect(app.quit)
 
     def worker(self) -> None:
-        """The worker for the encryption and decryption"""
         self.get_inputs()
         if self.password:
             self.ui.progress_bar.setValue(0)
             parts = round(100 / len(self.file_list))
-            enc_or_dec = self.check_enc_or_dec()
+            encryption = self.check_encryption()
             for i, file in enumerate(self.file_list):
-                if file.lower().endswith('.pdf') and enc_or_dec == 'enc':
-                    num = crypt.encrypt_pdf(file, self.password)
-                elif file.lower().endswith('.pdf') and enc_or_dec == 'dec':
-                    num = crypt.decrypt_pdf(file, self.password)
-                elif enc_or_dec == 'enc':
-                    num = crypt.encrypt(file, self.password)
-                else:
-                    num = crypt.decrypt(file, self.password)
-                if self.ui.settings.delete_files.isChecked() and num not in (101, 102):
+                try:
+                    Image.open(file).verify()
+                    status_code = ImageHandler(file=file, password=self.password, encryption=encryption).start()
+                except UnidentifiedImageError:
+                    if file.lower().endswith('.pdf'):
+                        status_code = PDFHandler(file=file, password=self.password, encryption=encryption).start()
+                    else:
+                        status_code = Crypt(file=file, password=self.password, encryption=encryption).start()
+
+                if self.ui.settings.delete_files.isChecked() and status_code not in (101, 102):
                     os.remove(file)
-                self.messages(num, file)
+                self.messages(status_code, file)
                 self.ui.progress_bar.setValue(parts * (i + 1))
             self.ui.progress_bar.setValue(100)
             self.messages(2, '')  # Finished
             self.clear_all()
 
     def get_inputs(self) -> None:
-        """Create the input paths and get the password"""
         paths = self.ui.drop_data.get_paths()
         if paths:
             self.sort_input(paths)
@@ -275,7 +214,6 @@ class Main:
             self.messages(106, "")  # No files selected
 
     def sort_input(self, input_list: list) -> None:
-        """Generate a file list from the input paths"""
         self.ui.message.clear()
         self.ui.pass_input.setEnabled(True)
         for path in input_list:
@@ -286,7 +224,6 @@ class Main:
                 self.file_list.append(path)
 
     def search_in_folder(self, paths: list) -> None:
-        """Search for files in folders"""
         while paths:
             under_paths = []
             for path in paths:
@@ -303,12 +240,10 @@ class Main:
 
     @staticmethod
     def extend_dir(path_list: list, path: str) -> list:
-        """Return a list with the full names of the files in the folder"""
         path_list = [path + "\\" + item for item in path_list]
         return path_list
 
     def get_password(self) -> None:
-        """Get password from user input"""
         self.password = self.ui.pass_input.text()
         if not self.check_password(self.password):
             self.messages(100, '')  # Password not valid
@@ -322,7 +257,6 @@ class Main:
 
     @staticmethod
     def check_password(password: str) -> bool:
-        """Return true if the password is valid"""
         special_sym = ['#', '%', '@', '$']
         val = True
         if len(password) < 10:
@@ -337,90 +271,88 @@ class Main:
             val = False
         return val
 
-    def check_enc_or_dec(self) -> str:
-        """Return the operation"""
+    def check_encryption(self) -> bool:
         if self.ui.enc.isChecked():
-            return 'enc'
-        return 'dec'
+            return True
+        return False
 
-    def messages(self, num: int, file: str) -> None:
-        """Create the message"""
+    def messages(self, status_code: int, file: str) -> None:
         file = os.path.basename(file)
         colour = ""
         msg = ""
-        if num == 1:
+        if status_code == 1:
             if self.ui.lang == "EN":
                 msg = "Password does match the requirements!"
             else:
                 msg = "Passwort entspricht den Kriterien!"
             colour = "green"
-        elif num == 2:
+        elif status_code == 2:
             if self.ui.lang == "EN":
                 msg = "\nOperation completed"
             else:
                 msg = "\nVorgang abgeschlossen!"
             colour = "green"
-        elif num == 3:
+        elif status_code == 3:
             if self.ui.lang == "EN":
                 msg = f"File {file} successfully decrypted!"
             else:
                 msg = f"Datei {file} erfolgreich entschlüsselt!"
             colour = "green"
-        elif num == 4:
+        elif status_code == 4:
             if self.ui.lang == "EN":
                 msg = f"File {file} successfully encrypted!"
             else:
                 msg = f"Datei {file} erfolgreich verschlüsselt!"
             colour = "green"
-        elif num == 100:
+        elif status_code == 100:
             if self.ui.lang == "EN":
                 msg = "Password does not match the requirements!"
             else:
                 msg = "Passwort entspricht nicht den Kriterien!"
             colour = "red"
-        elif num == 101:
+        elif status_code == 101:
             if self.ui.lang == "EN":
                 msg = f"The file {file} does not exist!"
             else:
                 msg = f"Die Datei {file} existiert nicht!"
             colour = "red"
-        elif num == 102:
+        elif status_code == 102:
             if self.ui.lang == "EN":
                 msg = "select encrypt or decrypt!"
             else:
                 msg = "verschlüsseln oder entschlüsseln auswählen!"
             colour = "red"
-        elif num == 103:
+        elif status_code == 103:
             if self.ui.lang == "EN":
                 msg = f"Password for the file {file} is wrong!"
             else:
                 msg = f"Passwort für Datei {file} ist falsch!"
             colour = "red"
-        elif num == 104:
+        elif status_code == 104:
             if self.ui.lang == "EN":
                 msg = f"File {file} is already encrypted!"
             else:
                 msg = f"Datei {file} ist bereits verschlüsselt!"
             colour = "red"
-        elif num == 105:
+        elif status_code == 105:
             if self.ui.lang == "EN":
                 msg = f"File {file} is not encrypted!"
             else:
                 msg = f"Datei {file} ist nicht verschlüsselt!"
             colour = "red"
-        elif num == 106:
+        elif status_code == 106:
             if self.ui.lang == "EN":
                 msg = "No files or folders selected!"
             else:
                 msg = "Keine Dateien oder Ordner ausgewählt!"
             colour = "red"
-        elif num == 107:
+        elif status_code == 107:
             if self.ui.lang == "EN":
                 msg = "Invalid path reference!"
             else:
                 msg = "Pfadreferenz ungültig!"
             colour = "red"
-        elif num == 108:
+        elif status_code == 108:
             if self.ui.lang == "EN":
                 msg = f"Missing permissions for the file {file}!"
             else:
@@ -429,7 +361,6 @@ class Main:
         self.set_colour(colour, msg)
 
     def set_colour(self, colour: str, msg: str) -> None:
-        """Set the colour of the message"""
         if colour == "red":
             msg = f'<span style=\" color: #ED422C;\">{msg}</span>'
         elif colour == "green":
@@ -437,18 +368,15 @@ class Main:
         self.ui.message.append(msg)
 
     def clear_all(self) -> None:
-        """Clear all for the next run"""
         self.ui.pass_input.clear()
         self.remove_all_items()
         self.file_list = []
 
     def remove_all_items(self) -> None:
-        """Remove all items from the QListWidget"""
         self.ui.drop_data.clear()
         self.ui.drop_data.paths = []
 
     def remove_selected_items(self) -> None:
-        """Remove all selected items from the QListWidget"""
         items = [self.ui.drop_data.item(x) for x in range(self.ui.drop_data.count())]
         for item in items:
             self.ui.drop_data.setCurrentItem(item)
@@ -458,12 +386,10 @@ class Main:
                 del self.ui.drop_data.paths[index]
 
     def show_info(self) -> None:
-        """Show the Info Dialog"""
         self.ui.info_dialog.close.clicked.connect(self.ui.info_dialog.accept)
         self.ui.info_dialog.exec_()
 
     def show_settings(self) -> None:
-        """Show the Settings Dialog"""
         self.ui.settings.close.clicked.connect(self.ui.settings.accept)
         self.ui.settings.language_slider.valueChanged.connect(self.ui.change_language)
         self.ui.settings.exec_()
